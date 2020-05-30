2bc2bd0dd98aa8fab19e5770e6b1f24d99b2d1af
Erick Teixeira <erickhenriquemota@gmail.com>
2020-05-30 09:28:06 -0300

    Altera uri de acesso ao banco

--- database/config/config.js
+++ database/config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL,
+    url: 'postgres://zlxgtkfsyblwcf:195027bb5f304cb4ea4b6faac6ab946db4d73506ff24b8c905b43706b407fa4c@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d7tdb8slo0auua',
     dialect: 'postgres',
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    url: 'postgres://zlxgtkfsyblwcf:195027bb5f304cb4ea4b6faac6ab946db4d73506ff24b8c905b43706b407fa4c@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d7tdb8slo0auua',
     dialect: 'postgres',
   },
   production: {
-    url: process.env.DATABASE_URL,
+    url: 'postgres://zlxgtkfsyblwcf:195027bb5f304cb4ea4b6faac6ab946db4d73506ff24b8c905b43706b407fa4c@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d7tdb8slo0auua',
     dialect: 'postgres',
   },
 }
--- database/index.js
+++ database/index.js
@@ -6,7 +6,7 @@ const PostosRetirada = require('./models/PostosRetirada');
 const Estoque = require('./models/Estoque');
 const Doacao = require('./models/Doacao');
 
-const connection = new Sequelize('postgres://byrpwrxt:S5k54r6vucZqt7jEWhOstuVpmzD4CR_o@ruby.db.elephantsql.com:5432/byrpwrxt');
+const connection = new Sequelize('postgres://zlxgtkfsyblwcf:195027bb5f304cb4ea4b6faac6ab946db4d73506ff24b8c905b43706b407fa4c@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d7tdb8slo0auua');
 
 User.init(connection);
 PostosRetirada.init(connection);
